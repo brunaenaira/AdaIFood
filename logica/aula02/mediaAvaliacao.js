30c4c7a44d98600aaa7342298706855224d142d8
brunaenaira <bru_ariane5@hotmail.com>
2023-11-06 19:17:25 -0300

    primeiro arquivo lógica de programação

---
+++ logica/aula02/mediaAvaliacao.js
@@ -0,0 +1,6 @@
+const oneStar = 2
+const twoStars = 15
+const threeStars = 18
+const fourStars = 25
+const fiveStars = 40
+console.log(((1 * oneStar) + (2 * twoStars) + (3 * threeStars) + (4 * fourStars) + (5 * fiveStars)) / 100)
